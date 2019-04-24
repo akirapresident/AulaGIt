a6abf60892611d2975c0d2ebe1d5177bb9044ef4
Akira Tsukamoto <akiratsukamoto13@gmail.com>
2019-04-24 08:42:28 -0300

    oie :)

---
+++ AulaGIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AulaGIt
+//
+//  Created by akira tsukamoto on 24/04/19.
+//  Copyright © 2019 akira tsukamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         print("Teste")
+        // Do any additional setup after loading the view.
+    }
+}
+
